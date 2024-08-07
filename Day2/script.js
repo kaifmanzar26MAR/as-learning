7e62f91b2db6ce779bcaafb2596983d5d94b6000
MD KAIF MANZAR <kaifmanzar321@gmail.com>
2024-08-07 16:28:09 +0530

    todatys learning

---
+++ Day2/script.js
@@ -0,0 +1,43 @@
+// const submitBtn=document.getElementById('submit');
+
+// submitBtn.addEventListener('click',()=>{
+//     const newDive= document.createElement('div');
+//     let name="aysha"
+//     newDive.innerHTML=`<h1> This is the ${name} tag</h1>`;
+//     // newDive.style.color="green";
+//     // document.body.appendChild(newDive);
+
+//     newDive.style.color="green";
+//     const containerDiv= document.getElementById('containerDive');
+//     containerDiv.appendChild(newDive);
+// })
+
+
+// // datatypes
+// //premitive datatypes
+// // number, string, boolean, null, undefined
+// // Refrece type
+// // object, array, function
+// let name1="aysha";
+
+// // if(name1==="aysha"){
+// //     console.log("yes")
+// // }else{
+// //     console.log("no")
+// // }
+// console.log(name1==="aysha" ? "yes" : "no")
+
+// const arr= [1,2,3,4,5,6];
+
+// // for(let i=0;i<arr.length; i++){
+// //     console.log(arr[i])
+// // }
+// // arr.forEach((e, i)=>(console.log(e, i)));
+
+// let i=0;
+// // while(i<arr.length){
+// //     console.log(arr[i])
+// //     i++;
+// // }
+
+// // arr.map((e)=>(console.log(e)))
