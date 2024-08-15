1323ac3685bba65f444d25edd369812c81e540d4
MD KAIF MANZAR <kaifmanzar321@gmail.com>
2024-08-15 19:42:56 +0530

    day 5

---
+++ Day5/script.js
@@ -0,0 +1,53 @@
+
+// let arr= ["kaif", "saif", "aysha", "mofi", "tanmay", "hasan", "aarif"]
+
+// const filterdArray= arr.filter((ele)=>{
+//     if(ele[0]==='a'){
+//         return ele;
+//     }
+// })
+
+// console.log(filterdArray)
+
+// let foodItems= [
+    // {
+    //     id:0,
+    //     category:"Dinner",
+    //     name:"Dal Roti"
+    // },
+//     {
+//         id:1,
+//         category:"Lunch",
+//         name:"Rice"
+//     },
+//     {
+//         id:2,
+//         category:"Breakfast",
+//         name:"Bread and Tea"
+//     },
+//     {
+//         id:3,
+//         category:"Snaks",
+//         name:"Chps"
+//     },
+//     {
+//         id:0,
+//         category:"Dinner",
+//         name:"Sabji"
+//     },
+    
+// ]
+
+// const query="Snaks"
+// const filterFood=foodItems.filter((food)=>{
+//     if(food.category===query){
+//         return food;
+//     }
+// })
+
+// console.log(filterFood)
+
+// every thing in js is an object
+
+
+let arr=[1,3,54,6,]
