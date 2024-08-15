46f3dc353e0109942f34f6bfb507c8c42e33ca74
MD KAIF MANZAR <kaifmanzar321@gmail.com>
2024-08-15 19:57:02 +0530

    day -5

--- Day5/script.js
+++ Day5/script.js
@@ -1,53 +1,73 @@
 
-// let arr= ["kaif", "saif", "aysha", "mofi", "tanmay", "hasan", "aarif"]
+// // let arr= ["kaif", "saif", "aysha", "mofi", "tanmay", "hasan", "aarif"]
 
-// const filterdArray= arr.filter((ele)=>{
-//     if(ele[0]==='a'){
-//         return ele;
-//     }
-// })
+// // const filterdArray= arr.filter((ele)=>{
+// //     if(ele[0]==='a'){
+// //         return ele;
+// //     }
+// // })
 
-// console.log(filterdArray)
+// // console.log(filterdArray)
 
-// let foodItems= [
-    // {
-    //     id:0,
-    //     category:"Dinner",
-    //     name:"Dal Roti"
-    // },
-//     {
-//         id:1,
-//         category:"Lunch",
-//         name:"Rice"
-//     },
-//     {
-//         id:2,
-//         category:"Breakfast",
-//         name:"Bread and Tea"
-//     },
-//     {
-//         id:3,
-//         category:"Snaks",
-//         name:"Chps"
-//     },
-//     {
-//         id:0,
-//         category:"Dinner",
-//         name:"Sabji"
-//     },
+// // let foodItems= [
+//     // {
+//     //     id:0,
+//     //     category:"Dinner",
+//     //     name:"Dal Roti"
+//     // },
+// //     {
+// //         id:1,
+// //         category:"Lunch",
+// //         name:"Rice"
+// //     },
+// //     {
+// //         id:2,
+// //         category:"Breakfast",
+// //         name:"Bread and Tea"
+// //     },
+// //     {
+// //         id:3,
+// //         category:"Snaks",
+// //         name:"Chps"
+// //     },
+// //     {
+// //         id:0,
+// //         category:"Dinner",
+// //         name:"Sabji"
+// //     },
     
-// ]
+// // ]
 
-// const query="Snaks"
-// const filterFood=foodItems.filter((food)=>{
-//     if(food.category===query){
-//         return food;
+// // const query="Snaks"
+// // const filterFood=foodItems.filter((food)=>{
+// //     if(food.category===query){
+// //         return food;
+// //     }
+// // })
+
+// // console.log(filterFood)
+
+// // every thing in js is an object
+
+// Array.prototype.GetFirst = function() {
+//     return this[0];
+// }
+// Array.prototype.GetKaif = function() {
+//    return this.findIndex((ele)=>(ele==="aysha"))
+// }
+
+
+// String.prototype.Reverse= function(){
+//     let str=""
+//     for(let i=this.length-1;i>=0; i--){
+//         str+=this[i]
 //     }
-// })
+//     return str;
+// }
 
-// console.log(filterFood)
+// let arr=[1,3,54,6,5]
+// let names=["aysha", "mofi", "kaif"]
+// console.log("aysha".Reverse())
 
-// every thing in js is an object
-
-
-let arr=[1,3,54,6,]
+// console.log(arr.GetFirst())
+// console.log(names.GetKaif())
